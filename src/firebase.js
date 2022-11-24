0df106755a085f8021f93002bc13325b10f43193
DmitryNikolaev <dmi3y_tsk@bk.ru>
2022-11-24 22:59:42 +0200

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDweNL4NQAUfR5-9P-5dkV5-2d6j2QfvCM",
+  authDomain: "todolist-test-task.firebaseapp.com",
+  projectId: "todolist-test-task",
+  storageBucket: "todolist-test-task.appspot.com",
+  messagingSenderId: "376597323610",
+  appId: "1:376597323610:web:94f44cacafecdda5935d63",
+};
+
+const app = initializeApp(firebaseConfig);
+const store = getFirestore(app);
+
+export { store };
