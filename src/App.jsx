744f7458cf5925f2015ccf3aa68de9225858794c
DmitryNikolaev <dmi3y_tsk@bk.ru>
2022-11-28 23:57:31 +0200

    add less

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Title from "./components/Title/Title";
 import AddTask from "./components/AddTask/AddTask";
 import TasksList from "./components/TasksList/TasksList";
 
+import "./App.less"
+
 const App = () => {
   return (
     <div className="todolist">
